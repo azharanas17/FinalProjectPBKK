0f6577e760f0a6414ec37345e73655259bf8c04b
Nama Anda <email@example.com>
2023-10-03 15:12:39 +0700

    Tambah Data

--- database/factories/BookFactory.php
+++ database/factories/BookFactory.php
@@ -19,30 +19,30 @@ class BookFactory extends Factory
                                                         'To Kill a Mockingbird',
                                                         'The Great Gatsby',
                                                         'One Hundred Years of Solitude',
-                                                        'The Catcher in the Rye'
-                                                        'Pride and Prejudice'
-                                                        'The Hobbit'
-                                                        'Brave New World'
-                                                        'The Lord of the Rings'
-                                                        'The Chronicles of Narnia'
-                                                        'Moby-Dick'
-                                                        'The Odysses'
-                                                        'Jane Eyre'
+                                                        'The Catcher in the Rye',
+                                                        'Pride and Prejudice',
+                                                        'The Hobbit',
+                                                        'Brave New World',
+                                                        'The Lord of the Rings',
+                                                        'The Chronicles of Narnia',
+                                                        'Moby-Dick',
+                                                        'The Odysses',
+                                                        'Jane Eyre',
                                                         'The Count of Monte Cristo']),
             'penulis' => $this->faker->randomElement(['J.K Rowling',
                                                         'Harper Lee',
                                                         'F. Scott Fitzgerald',
                                                         'William Faulkner',
-                                                        'J.D. Salinger'
-                                                        'Jane Austen'
-                                                        'J.R.R Tolkien'
-                                                        'J.R.R Tolkien'
-                                                        'C.S. Lewis'
-                                                        'Herman Melville'
-                                                        'Homer'
-                                                        'Charlotte Bronte'
-                                                        'Alexander Dumas'
-                                                        'Leo Tolstoy'
+                                                        'J.D. Salinger',
+                                                        'Jane Austen',
+                                                        'J.R.R Tolkien',
+                                                        'J.R.R Tolkien',
+                                                        'C.S. Lewis',
+                                                        'Herman Melville',
+                                                        'Homer',
+                                                        'Charlotte Bronte',
+                                                        'Alexander Dumas',
+                                                        'Leo Tolstoy',
                                                         'Paulo Coelho']),
             'tahun_terbit' => $this->faker->randomElement([1997, 1960, 1925, 1967, 1951, 1813, 1937, 1932, 1954, 1950, 1851, 1800, 1847, 1844, 1869]),
             'jumlah_buku' => $this->faker->randomElement([122, 77, 99, 323, 65, 34, 87, 12, 30, 45, 82, 132, 94, 56, 29 ]),
