2c7c80b20d8c78b3c18aef48e215e4cdc19697d1
azharanas173 <azharanas173@gmail.com>
2023-10-17 15:33:52 +0700

    Add Infrastructure

---
+++ app/Http/Module/Book/Infrastructure/Repository/BookRepository.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Module\Book\Infrastructure\Repository;
+
+use App\Http\Module\Product\Domain\Model\Book;
+use App\Http\Module\Product\Domain\Services\Repository\BookRepositoryInterface;
+use Illuminate\Support\Facades\DB;
+
+class BookRepository implements BookRepositoryInterface
+{
+    public function save(Book $book)
+    {
+        DB::table('books')->upsert(
+            [
+                'judul_buku' => $book->judul_buku,
+                'penulis' => $book->penulis,
+                'tahun_terbit' => $book->tahun_terbit,
+                'jumlah_buku' => $book->jumlah_buku,
+            ],'judul_buku'
+        );
+    }
+}
