8df7a5b90862c9ecbcbf3b9adb148477bd7a8511
azharanas173 <azharanas173@gmail.com>
2023-11-20 21:16:30 +0700

    feat: add view for edit

---
+++ resources/views/book/edit.blade.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>Edit Book</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <!-- include summernote css -->
+    <link href="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote.min.css" rel="stylesheet">
+</head>
+
+<body>
+
+    <div class="container mt-5 mb-5">
+        <div class="row">
+            <div class="col-md-12">
+
+                <!-- Notifikasi menggunakan flash session data -->
+                @if (session('success'))
+                <div class="alert alert-success">
+                    {{ session('success') }}
+                </div>
+                @endif
+
+                @if (session('error'))
+                <div class="alert alert-error">
+                    {{ session('error') }}
+                </div>
+                @endif
+
+                <div class="card border-0 shadow rounded">
+                    <div class="card-body">
+                        <form action="{{ route('book.update', $book->id) }}" method="POST">
+                            @csrf
+                            @method('PUT')
+
+                            <div class="form-group">
+                                <label for="judul_buku">Judul Buku</label>
+                                <input type="text" class="form-control @error('judul_buku') is-invalid @enderror"
+                                    name="judul_buku" value="{{ old('judul_buku', $book->judul_buku) }}" required>
+                            </div>
+
+                            <div class="form-group">
+                                <label for="penulis">Penulis</label>
+                                <input type="text" class="form-control @error('penulis') is-invalid @enderror"
+                                    name="penulis" value="{{ old('penulis', $book->penulis) }}" required>
+                            </div>
+
+                            <div class="form-group">
+                                <label for="tahun_terbit">Tahun Terbit</label>
+                                <input type="number" class="form-control @error('tahun_terbit') is-invalid @enderror"
+                                    name="tahun_terbit" value="{{ old('tahun_terbit', $book->tahun_terbit) }}" required>
+                            </div>
+
+                            <div class="form-group">
+                                <label for="jumlah_buku">Jumlah Buku</label>
+                                <input type="text" class="form-control @error('jumlah_buku') is-invalid @enderror"
+                                    name="jumlah_buku" value="{{ old('jumlah_buku', $book->jumlah_buku) }}" required>
+                            </div>
+
+                            <button type="submit" class="btn btn-md btn-primary">Update</button>
+                            <a href="{{ route('book.index') }}" class="btn btn-md btn-secondary">back</a>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"
+        integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous">
+    </script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <!-- include summernote js -->
+    <script src="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote.min.js"></script>
+    <script>
+        $(document).ready(function() {
+            $('#content').summernote({
+                height: 250, //set editable area's height
+            });
+        })
+    </script>
+</body>
+
+</html>
