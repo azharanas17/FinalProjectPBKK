fbb0cece567e896cb8cb2be7b0b86abeb1395e67
azharanas173 <azharanas173@gmail.com>
2023-11-20 14:53:22 +0700

    fix: typo input name

--- resources/views/book/create.blade.php
+++ resources/views/book/create.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>Create New Post - Tutorial CRUD Laravel 8 @ qadrlabs.com</title>
+    <title>Create New Book</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <!-- include summernote css -->
     <link href="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote.min.css" rel="stylesheet">
@@ -20,15 +20,15 @@
 
                 <!-- Notifikasi menggunakan flash session data -->
                 @if (session('success'))
-                <div class="alert alert-success">
-                    {{ session('success') }}
-                </div>
+                    <div class="alert alert-success">
+                        {{ session('success') }}
+                    </div>
                 @endif
 
                 @if (session('error'))
-                <div class="alert alert-error">
-                    {{ session('error') }}
-                </div>
+                    <div class="alert alert-error">
+                        {{ session('error') }}
+                    </div>
                 @endif
 
                 <div class="card border-0 shadow rounded">
@@ -38,16 +38,9 @@
                             @csrf
 
                             <div class="form-group">
-                                <label for="judul">Judul Buku</label>
-                                <input type="text" class="form-control @error('judul') is-invalid @enderror"
-                                    name="judul" value="{{ old('judul') }}" required>
-
-                                <!-- error message untuk judul -->
-                                @error('judul')
-                                <div class="invalid-feedback">
-                                    {{ $message }}
-                                </div>
-                                @enderror
+                                <label for="judul_buku">Judul Buku</label>
+                                <input type="text" class="form-control @error('judul_buku') is-invalid @enderror"
+                                    name="judul_buku" value="{{ old('judul_buku') }}" required>
                             </div>
 
                             <div class="form-group">
@@ -57,15 +50,15 @@
                             </div>
 
                             <div class="form-group">
-                                <label for="tahun">Tahun Terbit</label>
-                                <input type="number" class="form-control @error('tahun') is-invalid @enderror"
-                                    name="tahun" value="{{ old('tahun') }}" required>
+                                <label for="tahun_terbit">Tahun Terbit</label>
+                                <input type="number" class="form-control @error('tahun_terbit') is-invalid @enderror"
+                                    name="tahun_terbit" value="{{ old('tahun_terbit') }}" required>
                             </div>
 
                             <div class="form-group">
-                                <label for="jumlah">Jumlah Buku</label>
-                                <input type="text" class="form-control @error('jumlah') is-invalid @enderror"
-                                    name="jumlah" value="{{ old('jumlah') }}" required>
+                                <label for="jumlah_buku">Jumlah Buku</label>
+                                <input type="text" class="form-control @error('jumlah_buku') is-invalid @enderror"
+                                    name="jumlah_buku" value="{{ old('jumlah_buku') }}" required>
                             </div>
 
                             <button type="submit" class="btn btn-md btn-primary">Save</button>
